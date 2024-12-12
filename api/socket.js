1ce5eeb9c5c4f479d13e3cf169e4bc2347f1d1f2
DUB55 <tcookdub5@gmail.com>
2024-12-12 14:12:30 +0100

    Update socket.js

--- api/socket.js
+++ api/socket.js
@@ -10,15 +10,18 @@ export default function handler(req, res) {
     io.on("connection", (socket) => {
       console.log("A user connected");
 
+      // Listen for chat messages and broadcast them
       socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
+        io.emit("chat message", msg); // Broadcast message to all connected clients
       });
 
+      // Listen for user disconnections
       socket.on("disconnect", () => {
         console.log("A user disconnected");
       });
     });
 
+    // Save the socket server to avoid reinitialization on each request
     res.socket.server.io = io;
   }
 
